85a90c28d23e453c4819e78ecb3d05a66c1001dd
kaustubh0777 <kaustuap555@gmail.com>
2022-06-20 12:18:27 +0530

    first commit

---
+++ eventPage.js
@@ -0,0 +1,27 @@
+
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.contextMenus.create({
+      id: "JustWiki",
+      title: 'JustWikiit',
+      contexts: ["selection"]
+    });
+  });
+
+function fixedEncodeURI (str) {
+    return encodeURI(str).replace(/%5B/g, '[').replace(/%5D/g, ']');
+}
+
+chrome.contextMenus.onClicked.addListener(function(clickData){   
+    if (clickData.menuItemId == "JustWiki" && clickData.selectionText){    
+        var wikiUrl = "https://en.wikipedia.org/wiki/" + fixedEncodeURI(clickData.selectionText);
+        var createData = {
+            "url": wikiUrl,
+            "type": "popup",
+            "top": 5,
+            "left": 5,
+            "width": screen.availWidth/2,
+            "height": screen.availHeight/2
+        };              
+        chrome.windows.create(createData, function(){});        
+    }
+});
